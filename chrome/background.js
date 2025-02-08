78062de86ca434057d12459d677bf978b9b2efe8
unser229 <69352467+unser229@users.noreply.github.com>
2025-02-08 23:59:10 +0500

    update url

--- chrome/background.js
+++ chrome/background.js
@@ -2,13 +2,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "classify") {
         console.log("Запрос на классификацию получен:", request.text); // Лог входящего запроса
 
-        fetch("http://blackfoxus.ru/classify/", {
+        fetch("https://blackfoxus.ru:8000/classify/", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ text: request.text }),
         })
         .then(response => {
-            console.log("Полчен ответ http:", response);
+            console.log("Полчен ответ https:", response);
             if (!response.ok) {
                 console.log("!response.ok now")
                 throw new Error(`HTTP error! Status: ${response.status}`);
